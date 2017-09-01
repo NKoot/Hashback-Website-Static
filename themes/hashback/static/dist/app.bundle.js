42a2e903f767f877a90fc05722498ac98942489e
Nata Koot <natakoot@gmail.com>
2017-09-01 20:35:38 +0300

    collapsed menu for mobile

--- themes/hashback/static/dist/app.bundle.js
+++ themes/hashback/static/dist/app.bundle.js
@@ -58193,19 +58193,34 @@ var $ = __webpack_require__(147);
 
 
 $('body').animate({
-    scrollTop: $(window).scrollTop(0)
+	scrollTop: $(window).scrollTop(0)
 });
 
 $(window).scroll(function () {
-    $('.force3d').css('opacity', 1 - $(window).scrollTop() / 250);
-    $('.fadein').css('opacity', 0 + $(window).scrollTop() / 250);
+	$('.force3d').css('opacity', 1 - $(window).scrollTop() / 250);
+	$('.fadein').css('opacity', 0 + $(window).scrollTop() / 250);
 });
 
 $('.anchor-link').on('click', function (e) {
-    e.preventDefault();
-    $('body').animate({
-        scrollTop: $('.main_page_text').offset().top
-    }, 'slow');
+	e.preventDefault();
+	$('body').animate({
+		scrollTop: $('.main_page_text').offset().top
+	}, 'slow');
+});
+
+$('.js_collapsed_button').on('click', function (e) {
+	e.preventDefault();
+	$('.js_collapsed_menu').toggle();
+});
+
+$('.js_collapsed_menu a').on('click', function (e) {
+	$('.js_collapsed_menu').hide();
+});
+
+$('body').on('click', function (e) {
+	if (e.target.id !== 'js_collapsed_button' && e.target.nodeName !== 'NAV' && e.target.nodeName !== 'LI') {
+		$('.js_collapsed_menu').hide();
+	}
 });
 
 /***/ }),
